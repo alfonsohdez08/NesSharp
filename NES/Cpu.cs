6dafcafd51e09f8fbaa6d8b2612ae383957d3a93
Alfonso Hernandez <alfonsohdez08@hotmail.com>
2020-04-28 09:03:08 -0400

    wip: add 6502 cpu

---
+++ NES/Cpu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NES
+{
+    /// <summary>
+    /// The 6502 CPU.
+    /// </summary>
+    class Cpu
+    {
+        #region 8-bits registers
+        /// <summary>
+        /// Accumulators.
+        /// </summary>
+        private Register<byte> _a;
+
+        /// <summary>
+        /// X register (general purpose).
+        /// </summary>
+        private Register<byte> _x;
+
+        /// <summary>
+        /// Y register (general purpose).
+        /// </summary>
+        private Register<byte> _y;
+
+        /// <summary>
+        /// Status register (each bit represents a flag).
+        /// </summary>
+        private Register<byte> _status;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Register<byte> _stackPointer;
+        #endregion
+
+        #region 16-bits register
+        /// <summary>
+        /// The Program Counter register (holds the memory address of the next instruction).
+        /// </summary>
+        private Register<short> _programCounter;
+        #endregion
+
+        /// <summary>
+        /// Represents the CPU's memory whose size is 2KiB (2,048 bytes).
+        /// </summary>
+        private readonly byte[] _memory = new byte[2048]; //Memory are organized in ranges (mappers)
+
+
+    }
+}
---
+++ NES/Register.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NES
+{
+    /// <summary>
+    /// Represents a general register for the 6502 CPU.
+    /// </summary>
+    class Register<T> where T: struct
+    {
+        /// <summary>
+        /// Represents the value that a general register of a 6502 CPU can hold.
+        /// </summary>
+        private T _value;
+
+        /// <summary>
+        /// Allocates a value in the register.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        public void SetValue(T value) => _value = value;
+
+        /// <summary>
+        /// Retrieves the value allocated in the register.
+        /// </summary>
+        /// <returns>The value allocated in the register.</returns>
+        public T GetValue() => _value;
+    }
+}
