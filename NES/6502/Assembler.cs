b663f267dc1ea14517acbaf08a1f61a4b2d15555
Alfonso Hernandez <alfonsohdez08@hotmail.com>
2020-05-24 11:51:40 -0400

    made a simple assembler :)

--- NES/6502/Assembler.cs
+++ NES/6502/Assembler.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
 namespace NES._6502
 {
-    class Assembler
+    static class Assembler
     {
         private static readonly Dictionary<string, HashSet<AddressingMode>> MnemonicAddrModes;
 
@@ -16,19 +17,19 @@ namespace NES._6502
             {AddressingMode.Absolute, new Regex(@"\$([0-9]|[a-f]){1,4}(\r\n|\n)", RegexOpts)},
             {AddressingMode.AbsoluteX, new Regex(@"\$([0-9]|[a-f]){1,4},x(\r\n|\n)", RegexOpts)},
             {AddressingMode.AbsoluteY, new Regex(@"\$([0-9]|[a-f]){1,4},y(\r\n|\n)", RegexOpts)},
-            {AddressingMode.Immediate, new Regex(@"#\$([0-9]|[a-f]){1,2}(\r\n|\n)", RegexOpts)},
+            {AddressingMode.Immediate, new Regex(@"^#\$([0-9]|[a-f]){1,2}(\r\n|\n)", RegexOpts)},
             {AddressingMode.Implied, null},
             {AddressingMode.Indirect, new Regex(@"\(\$([0-9]|[a-f]){1,4}\)(\r\n|\n)", RegexOpts)},
             {AddressingMode.IndirectX, new Regex(@"\(\$([0-9]|[a-f]){1,2},x\)(\r\n|\n)", RegexOpts)},
             {AddressingMode.IndirectY, new Regex(@"\(\$([0-9]|[a-f]){1,2}\),y(\r\n|\n)", RegexOpts)},
-            {AddressingMode.Relative, new Regex(@"\$([0-9]|[a-f]){1,2}(\r\n|\n)", RegexOpts)},
-            {AddressingMode.ZeroPage, new Regex(@"\$([0-9]|[a-f]){1,2}(\r\n|\n)", RegexOpts)},
-            {AddressingMode.ZeroPageX, new Regex(@"\$([0-9]|[a-f]){1,2},x(\r\n|\n)", RegexOpts)},
-            {AddressingMode.ZeroPageY, new Regex(@"\$([0-9]|[a-f]){1,2},y(\r\n|\n)", RegexOpts)},
+            {AddressingMode.Relative, new Regex(@"^\$([0-9]|[a-f]){1,2}(\r\n|\n)", RegexOpts)},
+            {AddressingMode.ZeroPage, new Regex(@"^\$([0-9]|[a-f]){1,2}(\r\n|\n)", RegexOpts)},
+            {AddressingMode.ZeroPageX, new Regex(@"^\$([0-9]|[a-f]){1,2},x(\r\n|\n)", RegexOpts)},
+            {AddressingMode.ZeroPageY, new Regex(@"^\$([0-9]|[a-f]){1,2},y(\r\n|\n)", RegexOpts)},
         };
 
-        private static readonly Regex _16BitRegexPattern = new Regex(@"([0-9]|[a-f]){1,4}");
-        private static readonly Regex _8BitRegexPattern = new Regex(@"([0-9]|[a-f]){1,2}");
+        private static readonly Regex _16BitRegexPattern = new Regex(@"([0-9]|[a-f]){1,4}", RegexOpts);
+        private static readonly Regex _8BitRegexPattern = new Regex(@"([0-9]|[a-f]){1,2}", RegexOpts);
 
         static Assembler()
         {
@@ -51,15 +52,19 @@ namespace NES._6502
         /// </summary>
         /// <param name="program">The program.</param>
         /// <returns>A set of hexadecimal values where each value represents either an instruction or an instruction operand.</returns>
-        public string[] Assemble(string program)
+        public static string[] Assemble(string program)
         {
             var programAssembled = new List<string>();
 
-            string[] lines = program.Split('\n');
+            string[] lines = program.Split("\r\n");
             for (int i = 0; i < lines.Length; i++)
             {
                 string line = lines[i];
+                if (string.IsNullOrEmpty(line) || string.IsNullOrWhiteSpace(line))
+                    continue;
+
                 line = line.Split(';')[0]; // Strips the comments next to the instruction (a comment is started by using semi colon)
+                line += '\n'; // Add new line feed
 
                 string[] instructionHex = ParseInstruction(line);
                 programAssembled.AddRange(instructionHex);
@@ -80,10 +85,12 @@ namespace NES._6502
             if (tokens.Length > 2)
                 throw new ArgumentException("The instruction isn't delimitted correctly. Ensure you specify a single whitespace between the mnemonic and the instruction operand.");
 
-            string mnemonic = tokens[0];
+            string mnemonic = tokens[0].ToUpper();
 
-            // Operand would not be supplied when using implied addressing mode
-            string operand = tokens[1];
+            string operand = string.Empty;
+            if (tokens.Length > 1) // Operand would not be supplied when using implied addressing mode
+                operand = tokens[1];
+
             AddressingMode addressingMode = ParseAddressingMode(mnemonic, operand);
 
             string instructionHex = GetInstructionHexValue(mnemonic, addressingMode);
@@ -122,6 +129,7 @@ namespace NES._6502
                         //byte lowByte = (byte)(val & 0xFF);
                         //byte highByte = (byte)(val >> 8 & 0xFF);
 
+                        // 6502 CPU is little endian (low byte first and then high byte)
                         hexValues.Add(lowByte.ToString("x"));
                         hexValues.Add(highByte.ToString("x"));
                     }
@@ -148,9 +156,12 @@ namespace NES._6502
             if (!MnemonicAddrModes.ContainsKey(mnemonic))
                 throw new ArgumentException($"The given mnemonic {mnemonic} does not exist.");
 
-            operand = StripSpaces(operand);
+            //operand = StripSpaces(operand);
 
             HashSet<AddressingMode> addrModes = MnemonicAddrModes[mnemonic];
+            if (addrModes.Count == 1)
+                return addrModes.First();
+
             foreach (AddressingMode addrMode in addrModes)
             {
                 Regex regex = AddrModeRegexPatterns[addrMode];
@@ -161,49 +172,6 @@ namespace NES._6502
             static string StripSpaces(string s) => s.TrimStart().TrimEnd();
 
             throw new InvalidOperationException($"Can not identify the addressing mode for the mnemonic {mnemonic} based on the provided operand {operand}");
-        }
-    }
-
-    class CpuInstruction : Instruction
-    {
-        /// <summary>
-        /// The representation of the instruction in hexadecimal.
-        /// </summary>
-        public string InstructionHex => InstructionDecimal.ToString("X");
-
-        /// <summary>
-        /// The representation of the instruction in decimal.
-        /// </summary>
-        public int InstructionDecimal { get; private set; }
-
-        public string[] GetHexInstruction()
-        {
-            throw new NotImplementedException();
-        }
-
-        private string[] ParseOperand(string operand, AddressingMode addressingMode)
-        {
-            operand = StripDollarSign(operand);
-            var hexValues = new List<string>();
-
-            switch (addressingMode)
-            {
-                case AddressingMode.Immediate:
-                case AddressingMode.ZeroPage:
-                case AddressingMode.ZeroPageX:
-                case AddressingMode.ZeroPageY:
-                    hexValues.Add(operand);
-                    break;
-            }
-
-            static string StripDollarSign(string s) => s.Replace("$", "");
-
-            return hexValues.ToArray();
-        }
-
-        public CpuInstruction(Instruction instruction, int instructionDecimalValue, string operand): base(instruction.Mnemonic, instruction.AddressingMode, instruction.MachineCycles)
-        {
-            InstructionDecimal = instructionDecimalValue;
         }
     }
 }
--- NES/Program.cs
+++ NES/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NES._6502;
+using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace NES
 {
@@ -9,24 +11,30 @@ namespace NES
         {
             //string[] hexDump = new string[] { "a9", "01" ,"8d", "00", "02", "a9" ,"05" ,"8d" ,"01" ,"02" ,"a9" ,"08", "8d" ,"02" ,"02" };
             //string program = "a9 88 38 e9 09";
-            string program = "a9 88 38 e9 f7";
-            string[] hexDump = program.Split(' ');
-            //string[] hexDump = new string[] { "a9", "01", "e9","78" };
-            var memory = new Memory();
-            const ushort startingAddress = 0x0001;
-            ushort address = startingAddress; //initial address
+            //string program = "a9 88 38 e9 f7";
+            //string[] hexDump = program.Split(' ');
+            ////string[] hexDump = new string[] { "a9", "01", "e9","78" };
+            //var memory = new Memory();
+            //const ushort startingAddress = 0x0001;
+            //ushort address = startingAddress; //initial address
 
-            for (int i = 0; i < hexDump.Length; i++)
-            {
-                byte b = Convert.ToByte(hexDump[i], 16);
+            //for (int i = 0; i < hexDump.Length; i++)
+            //{
+            //    byte b = Convert.ToByte(hexDump[i], 16);
 
-                memory.Store(address, b);
-                address++;
-            }
+            //    memory.Store(address, b);
+            //    address++;
+            //}
 
-            var cpu = new Cpu(memory, startingAddress);
+            //var cpu = new Cpu(memory, startingAddress);
 
-            cpu.Start();
+            //cpu.Start();
+
+            var sb = new StringBuilder();
+            sb.AppendLine("lda #$01");
+            sb.AppendLine("sta $10A");
+
+            string[] hexDump = Assembler.Assemble(sb.ToString());
 
             Console.ReadLine();
         }
